74dea60a9b30ae2cbf8fcdeaa67688a1dc151575
Lucas Sales <lucassales398@gmail.com>
2020-07-06 21:25:54 -0300

    first commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PageHeader from "../template/pageHeader";
+
+export default (props) => (
+  <div>
+    <PageHeader name="Sobre" small="Nós" />
+    <h2>Nossa História</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas unde ab
+      atque reprehenderit ipsa iure! Ex voluptatibus ducimus quidem odio omnis
+      sit, explicabo distinctio saepe, eligendi tempora deserunt eum pariatur!
+    </p>
+    <h2>Missão e Visão</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis nulla
+      modi deserunt dolorem reiciendis dolore corporis enim tempora odit
+      mollitia perspiciatis, ut soluta, fugiat, in iusto velit repellendus. Cum,
+      ex.
+    </p>
+    <h2>Imprensa</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+      exercitationem nam sint cumque nulla repellat unde? Rem consectetur
+      molestiae fugit repudiandae, optio unde excepturi minus omnis amet, natus
+      maxime quis.
+    </p>
+  </div>
+);
